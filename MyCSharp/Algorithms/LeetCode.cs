f2eb771048f41674593e7e6f95f21505967be057
Krain <1115114214@qq.com>
2019-02-19 11:35:41 +0800

    add expression match

---
+++ MyCSharp/Algorithms/LeetCode.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCSharp.Algorithms
+{
+    public static class LeetCode
+    {
+
+        /// <summary>
+        /// 10. Regular Expression Matching
+        /// Given an input string (s) and a pattern (p), implement regular expression matching with support for '.' and '*'
+        /// </summary>
+        public static bool IsMatch(string s, string p)
+        {
+            if (string.IsNullOrEmpty(p))
+            {
+                return string.IsNullOrEmpty(s);
+            }
+
+            bool firstMatch = s.Length > 0 && (s[0] == p[0] || p[0] == '.');
+
+            // deal with '*'
+            if (p.Length >= 2 && p[1] == '*')
+            {
+                return (firstMatch && IsMatch(s.Substring(1), p)) || (IsMatch(s, p.Substring(2)));
+            }
+
+            return firstMatch && IsMatch(s.Substring(1), p.Substring(1));
+        }
+
+        public static bool IsMatchDP(string s, string p)
+        {
+            int sLength = s.Length;
+            int pLength = p.Length;
+            // i, j     s [i,], p [j,]
+            bool[,] results = new bool[sLength, pLength];
+            results[sLength, pLength] = true;
+
+            for (int i = sLength; i >= 0; i--)
+            {
+                for (int j = pLength - 1; j >= 0; j--)
+                {
+                    bool firstMatch = i < sLength && (s[i] == p[j] || p[j] == '.');
+
+                    // deal with '*'
+                    if (j + 1 < pLength && p[j + 1] == '*')
+                    {
+                        results[i, j] = (firstMatch && results[i + 1, j]) || (results[i, j + 2]);
+                    }
+
+                    results[i, j] = firstMatch && results[i + 1, j + 1];
+                }
+            }
+
+            return results[0, 0];
+
+            //bool firstMatch = s.Length > 0 && (s[0] == p[0] || p[0] == '.');
+
+            //// deal with '*'
+            //if (p.Length >= 2 && p[1] == '*')
+            //{
+            //    return (firstMatch && IsMatch(s.Substring(1), p)) || (IsMatch(s, p.Substring(2)));
+            //}
+
+            //return firstMatch && IsMatch(s.Substring(1), p.Substring(1));
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ MyCSharpTests1/Algorithms/LeetCodeTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyCSharp.Algorithms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCSharp.Algorithms.Tests
+{
+    [TestClass()]
+    public class LeetCodeTests
+    {
+        [TestMethod()]
+        public void IsMatchTest()
+        {
+            Assert.IsTrue(LeetCode.IsMatch("aaa", "a.*"));
+            Assert.IsTrue(LeetCode.IsMatch("aaa", "aaa"));
+            Assert.IsTrue(LeetCode.IsMatch("aaab", "a*b"));
+            Assert.IsTrue(LeetCode.IsMatch("aaab", ".*"));
+            Assert.IsFalse(LeetCode.IsMatch("aaab", ".*c"));
+        }
+    }
+}
