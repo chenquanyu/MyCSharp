fd91e0d221e80a44a4368e602d708fcb9d653109
Krain <1115114214@qq.com>
2019-03-07 21:06:04 +0800

    更新LeetCode

--- MyCSharp/Algorithms/LeetCode.cs
+++ MyCSharp/Algorithms/LeetCode.cs
@@ -6,14 +6,14 @@ using System.Threading.Tasks;
 
 namespace MyCSharp.Algorithms
 {
-    public static class LeetCode
+    public class LeetCode
     {
 
         /// <summary>
         /// 10. Regular Expression Matching
         /// Given an input string (s) and a pattern (p), implement regular expression matching with support for '.' and '*'
         /// </summary>
-        public static bool IsMatch(string s, string p)
+        public bool IsMatch(string s, string p)
         {
             if (string.IsNullOrEmpty(p))
             {
@@ -31,7 +31,7 @@ namespace MyCSharp.Algorithms
             return firstMatch && IsMatch(s.Substring(1), p.Substring(1));
         }
 
-        public static bool IsMatchDP(string s, string p)
+        public bool IsMatchDP(string s, string p)
         {
             int sLength = s.Length;
             int pLength = p.Length;
@@ -65,7 +65,7 @@ namespace MyCSharp.Algorithms
         /// 29. Divide Two Integers
         /// Given two integers dividend and divisor, divide two integers without using multiplication, division and mod operator.
         /// </summary>
-        public static int Divide(int dividend, int divisor)
+        public int Divide(int dividend, int divisor)
         {
             if (dividend == int.MinValue && divisor == -1)
             {
@@ -90,7 +90,7 @@ namespace MyCSharp.Algorithms
         }
 
 
-        public static int CountHeight(int[] A)
+        public int CountHeight(int[] A)
         {
             if (A == null)
             {
@@ -144,7 +144,185 @@ namespace MyCSharp.Algorithms
             return maxPeriod;
         }
 
+        /// <summary>
+        /// 44. Wildcard Matching
+        /// Given an input string (s) and a pattern (p), implement wildcard pattern matching with support for '?' and '*'.
+        /// </summary>
+        public bool IsMatchWild(string s, string p)
+        {
+            if (string.IsNullOrEmpty(p))
+            {
+                return string.IsNullOrEmpty(s);
+            }
 
+            // shorten p
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < p.Length; i++)
+            {
+                if (i + 1 < p.Length && p[i] == '*' && p[i + 1] == '*')
+                {
+                    continue;
+                }
+                else
+                {
+                    builder.Append(p[i]);
+                }
+            }
+
+            p = builder.ToString();
+
+            int sLength = s == null ? 0 : s.Length;
+            int pLength = p.Length;
+
+            // s[i,] p[j,]
+            bool[,] dp = new bool[sLength + 1, pLength + 1];
+            dp[sLength, pLength] = true;
+
+            for (int i = sLength; i >= 0; i--)
+                for (int j = pLength - 1; j >= 0; j--)
+                {
+                    bool firstMatch = i < sLength && (s[i] == p[j] || p[j] == '?');
+
+                    if (p[j] == '*')
+                    {
+                        dp[i, j] = (i < sLength && dp[i + 1, j]) || dp[i, j + 1];
+                    }
+                    else
+                    {
+                        dp[i, j] = firstMatch && dp[i + 1, j + 1];
+                    }
+                }
+
+
+            return dp[0, 0];
+
+
+            //for (int i = 0; i < p.Length - 1; i++)
+            //{
+            //    if (p[i] == '*' && p[i + 1] == '*')
+            //    {
+            //        p = p.Substring(1);
+            //    }
+            //    else
+            //    {
+            //        break;
+            //    }
+            //}
+
+            //bool firstMatch = s.Length > 0 && (s[0] == p[0] || p[0] == '?');
+
+            //if (p[0] == '*')
+            //{
+            //    return (s.Length > 0 && IsMatch(s.Substring(1), p)) || IsMatch(s, p.Substring(1));
+            //}
+            //else
+            //{
+            //    return firstMatch && IsMatch(s.Substring(1), p.Substring(1));
+            //}
+
+        }
+
+        /// <summary>
+        /// 48. Rotate Image
+        /// You are given an n x n 2D matrix representing an image.
+        /// Rotate the image by 90 degrees(clockwise).
+        /// </summary>
+        public void Rotate(int[,] matrix)
+        {
+            int n = matrix.GetUpperBound(0) + 1;
+
+            int half = n / 2;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    Swap(matrix, i, j, j, i);
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < half; j++)
+                {
+                    Swap(matrix, i, j, i, n - 1 - j);
+                }
+            }
+
+        }
+
+        public void Swap(int[,] matrix, int a, int b, int a1, int b1)
+        {
+            int temp = matrix[a, b];
+            matrix[a, b] = matrix[a1, b1];
+            matrix[a1, b1] = temp;
+        }
+
+        public IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            Dictionary<string, List<string>> dict = new Dictionary<string, List<string>>();
+            foreach (var item in strs)
+            {
+                string key = new string(item.OrderBy(p => p).ToArray());
+
+                if (dict.ContainsKey(key))
+                {
+                    dict[key].Add(item);
+                }
+                else
+                {
+                    dict.Add(key, new List<string> { item });
+                }
+            }
+
+            var result = new List<IList<string>>();
+            foreach (var item in dict)
+            {
+                result.Add(item.Value);
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// x ^ n = (x ^ 2) ^ (n/2)
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public double MyPow(double x, int n)
+        {
+            if (x == 0)
+            {
+                return 0;
+            }
+
+            long k = n;
+            if (k < 0)
+            {
+                x = 1/x;
+                k = -k;
+            }
+
+            double result = 1;
+            double current = x;
+            while (k > 0)
+            {
+                bool odd = k % 2 == 1;
+
+                if (odd)
+                {
+                    result *= current;
+                }
+
+                current *= current;
+                k = k / 2;
+            }
+
+            return result;
+
+
+        }
 
     }
 }
--- MyCSharpTests1/Algorithms/LeetCodeTests.cs
+++ MyCSharpTests1/Algorithms/LeetCodeTests.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MyCSharp.Algorithms;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,39 +12,73 @@ namespace MyCSharp.Algorithms.Tests
     [TestClass()]
     public class LeetCodeTests
     {
+        private LeetCode leetCode = new LeetCode();
+
         [TestMethod()]
         public void IsMatchTest()
         {
-            Assert.IsTrue(LeetCode.IsMatch("aaa", "a.*"));
-            Assert.IsTrue(LeetCode.IsMatch("aaa", "aaa"));
-            Assert.IsTrue(LeetCode.IsMatch("aaab", "a*b"));
-            Assert.IsTrue(LeetCode.IsMatch("aaab", ".*"));
-            Assert.IsFalse(LeetCode.IsMatch("aaab", ".*c"));
+            Assert.IsTrue(leetCode.IsMatch("aaa", "a.*"));
+            Assert.IsTrue(leetCode.IsMatch("aaa", "aaa"));
+            Assert.IsTrue(leetCode.IsMatch("aaab", "a*b"));
+            Assert.IsTrue(leetCode.IsMatch("aaab", ".*"));
+            Assert.IsFalse(leetCode.IsMatch("aaab", ".*c"));
         }
 
         [TestMethod()]
         public void IsMatchDPTest()
         {
-            Assert.IsTrue(LeetCode.IsMatchDP("aaa", "a.*"));
-            Assert.IsTrue(LeetCode.IsMatchDP("aaa", "aaa"));
-            Assert.IsTrue(LeetCode.IsMatchDP("aaab", "a*b"));
-            Assert.IsTrue(LeetCode.IsMatchDP("aaab", ".*"));
-            Assert.IsFalse(LeetCode.IsMatchDP("aaab", ".*c"));
+            Assert.IsTrue(leetCode.IsMatchDP("aaa", "a.*"));
+            Assert.IsTrue(leetCode.IsMatchDP("aaa", "aaa"));
+            Assert.IsTrue(leetCode.IsMatchDP("aaab", "a*b"));
+            Assert.IsTrue(leetCode.IsMatchDP("aaab", ".*"));
+            Assert.IsFalse(leetCode.IsMatchDP("aaab", ".*c"));
         }
 
         [TestMethod()]
         public void CountHeightTest()
         {
-            Assert.AreEqual(0, LeetCode.CountHeight(new int[] { }));
-            Assert.AreEqual(1, LeetCode.CountHeight(new int[] { 100, 100 }));
-            Assert.AreEqual(2, LeetCode.CountHeight(new int[] { 100, 101 }));
-            Assert.AreEqual(2, LeetCode.CountHeight(new int[] { 9, 9, 2 }));
-            Assert.AreEqual(2, LeetCode.CountHeight(new int[] { 9, 9, 10 }));
-            Assert.AreEqual(2, LeetCode.CountHeight(new int[] { 2, 9, 9 }));
-            Assert.AreEqual(2, LeetCode.CountHeight(new int[] { 10, 9, 9 }));
-            Assert.AreEqual(5, LeetCode.CountHeight(new int[] { 10, 9, 8, 9, 8, 9 }));
-            Assert.AreEqual(5, LeetCode.CountHeight(new int[] { 10, 9, 8, 9, 8, 9, 10 }));
+            Assert.AreEqual(0, leetCode.CountHeight(new int[] { }));
+            Assert.AreEqual(1, leetCode.CountHeight(new int[] { 100, 100 }));
+            Assert.AreEqual(2, leetCode.CountHeight(new int[] { 100, 101 }));
+            Assert.AreEqual(2, leetCode.CountHeight(new int[] { 9, 9, 2 }));
+            Assert.AreEqual(2, leetCode.CountHeight(new int[] { 9, 9, 10 }));
+            Assert.AreEqual(2, leetCode.CountHeight(new int[] { 2, 9, 9 }));
+            Assert.AreEqual(2, leetCode.CountHeight(new int[] { 10, 9, 9 }));
+            Assert.AreEqual(5, leetCode.CountHeight(new int[] { 10, 9, 8, 9, 8, 9 }));
+            Assert.AreEqual(5, leetCode.CountHeight(new int[] { 10, 9, 8, 9, 8, 9, 10 }));
 
+        }
+
+
+        [TestMethod()]
+        public void SubstringTest()
+        {
+            Assert.AreEqual(string.Empty, "1".Substring(1));
+        }
+
+        [TestMethod()]
+        public void IsMatchWildTest()
+        {
+            Assert.AreEqual(true, leetCode.IsMatchWild("aa", "****a****?"));
+        }
+
+        [TestMethod()]
+        public void RotateTest()
+        {
+            var matrix = JsonConvert.DeserializeObject<int[,]>("[[1,2,3],[4,5,6],[7,8,9]]");
+            leetCode.Rotate(matrix);
+            Assert.AreEqual(7, matrix[0, 0]);
+        }
+
+        [TestMethod()]
+        public void MyPowTest()
+        {
+            Assert.AreEqual(0, leetCode.MyPow(2, int.MinValue));
+            Assert.AreEqual(1024, leetCode.MyPow(2, 10));
+            Assert.AreEqual(0.25, leetCode.MyPow(2, -2));
+            Assert.AreEqual(16, leetCode.MyPow(2, 4));
+
+            // Assert.Fail();
         }
     }
 }
