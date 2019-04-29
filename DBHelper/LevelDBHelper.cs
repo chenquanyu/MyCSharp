a6c2c292136f38892331b6563f689d9d8608420c
Krain Chen <chenquanyu@neo.org>
2019-04-29 15:19:25 +0800

    Add test for leveldb

---
+++ DBHelper/LevelDBHelper.cs
@@ -0,0 +1,45 @@
+﻿using Neo.IO.Data.LevelDB;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DBHelper
+{
+    public class LevelDBHelper
+    {
+        private readonly DB leveldb;
+
+        public LevelDBHelper(string path)
+        {
+            leveldb = DB.Open(path, new Options { CreateIfMissing = true });
+        }
+
+        public void Put(string key, string value)
+        {
+            var bkey = Encoding.UTF8.GetBytes(key);
+            var bvalue = Encoding.UTF8.GetBytes(value);
+            leveldb.Put(WriteOptions.Default, bkey, bvalue);
+        }
+
+        public string Get(string key)
+        {
+            var bkey = Encoding.UTF8.GetBytes(key);
+            var bvalue = leveldb.Get(ReadOptions.Default, bkey).ToArray();
+            return Encoding.UTF8.GetString(bvalue);
+        }
+
+        public void Write(KeyValuePair<string, string>[] kvs)
+        {
+            var batch = new WriteBatch();
+            foreach (var item in kvs)
+            {
+                var bkey = Encoding.UTF8.GetBytes(item.Key);
+                var bvalue = Encoding.UTF8.GetBytes(item.Value);
+                batch.Put(bkey, bvalue);
+            }
+            leveldb.Write(WriteOptions.Default, batch);
+        }
+
+
+    }
+}
---
+++ MyCore/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using DBHelper;
+using System;
+
+namespace MyCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var helper = new LevelDBHelper(@"D:\temp\leveldb");
+
+            System.Diagnostics.Stopwatch sp = new System.Diagnostics.Stopwatch();
+            sp.Start();
+            long mCount = 0;
+            while (true)
+            {
+                helper.Put(Guid.NewGuid().ToString(), "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaeraaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb");
+                if (System.Threading.Interlocked.Increment(ref mCount) % 10000 == 0)
+                {
+                    Console.WriteLine("{0} has inserted. time use {1}ms. speed {2} rpms", mCount, sp.ElapsedMilliseconds, mCount / sp.ElapsedMilliseconds);
+                }
+            }
+        }
+    }
+}
